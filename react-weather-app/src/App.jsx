97fe913151570c72b37c53e4e137d06d026a4cfb
asandamhlongo <156101659+asandamhlongo@users.noreply.github.com>
2024-04-22 15:47:00 +0200

    Added bootstrap and created weather component and its stylesheet

---
+++ react-weather-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+    <div className="App">
+        <div className="container">
+            <h1>Weather App</h1>
+            <Weather />
+            <footer>
+                This project was coded by Asanda Mhlongo and is open-sorced on GitHub.
+            </footer>
+        </div>
+    </div>
+  
+}
+
+export default App
---
+++ react-weather-app/src/Weather.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css"
+
+function Weather() {
+    return(
+        <div className="Weather">
+            <h1>Lisbon</h1>
+            <p>Wednesday 07:00</p>
+            <p>Mostly cloudy</p>
+        </div>
+    );
+}
+
+export default Weather
