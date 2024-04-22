7a1fd7d189fd9c1473f9ea2efe5ec8a0987b3eea
asandamhlongo <156101659+asandamhlongo@users.noreply.github.com>
2024-04-22 16:04:26 +0200

    Updated the App component by adding the weather content

--- react-weather-app/src/App.jsx
+++ react-weather-app/src/App.jsx
@@ -2,17 +2,43 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
-function App() {
+export default function App() {
+  return (
     <div className="App">
-        <div className="container">
-            <h1>Weather App</h1>
-            <Weather />
-            <footer>
-                This project was coded by Asanda Mhlongo and is open-sorced on GitHub.
-            </footer>
-        </div>
-    </div>
-  
-}
+      <div className="container">
+        <Weather defaultCity="New York" />
 
-export default App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Asanda Mhlongo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+  
+
+
+
--- react-weather-app/src/Weather.jsx
+++ react-weather-app/src/Weather.jsx
@@ -4,9 +4,23 @@ import "./Weather.css"
 function Weather() {
     return(
         <div className="Weather">
+            <form>
+                <input type="search"/>
+            </form>
             <h1>Lisbon</h1>
             <p>Wednesday 07:00</p>
             <p>Mostly cloudy</p>
+            <div className="row">
+                <div className="col-6>">
+                  <p>🌤 6°C</p>  
+                </div>
+                <div className="col-6">
+                    <p>Preception: 15%</p>
+                    <p>Humidity: 72%</p>
+                    <p>Wind: 13 km/h</p>
+
+                </div>
+            </div>
         </div>
     );
 }
